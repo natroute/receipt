0c7a6c6ad9e5398d1393166f178e8b3d8278fa92
natroute <247222395+natroute@users.noreply.github.com>
2026-04-19 19:37:32 +0300

    requirements

--- Run.ps1
+++ Run.ps1
@@ -3,9 +3,9 @@ if (!(Test-Path -Path '.venv'))
 	Write-Output ('Using ' + (python --version))
 	Write-Output 'Creating venv at ./.venv'
 	python -m venv .venv
-	Write-Output 'Installing dependencies'
-	.venv\Scripts\python.exe -m pip install -r requirements.txt
 }
 
+Write-Output 'Installing dependencies (if necessary)'
+.venv\Scripts\python.exe -m pip install -r requirements.txt
 Write-Output 'Running script'
 .venv\Scripts\python.exe main.py
